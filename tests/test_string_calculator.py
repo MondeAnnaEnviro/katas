acea06e415af4eda03951186432e2dc03aa3f386
Monde Anna <monde.anna@enviro365.co.za>
2025-06-27 08:17:43 +0200

    sum n numbers string

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -11,3 +11,7 @@ def test_single_number_returned_as_int():
 
 def test_doubley_numbered_string_returns_sum_as_int():
     assert add( "2,3" ) == 5
+
+
+def test_n_numbered_string_summation():
+    assert add( "1,2,3,4" ) == 10
