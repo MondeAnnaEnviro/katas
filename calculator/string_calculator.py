58f684719a59cc4282bf56d3771c9546a2c034f8
Monde Anna <monde.anna@enviro365.co.za>
2025-06-27 08:13:01 +0200

    empty string returns zero

---
+++ calculator/string_calculator.py
@@ -0,0 +1,4 @@
+
+
+def add( numbers: str ) -> int:
+    return 0
---
+++ tests/test_string_calculator.py
@@ -0,0 +1,6 @@
+from calculator.string_calculator import add
+
+
+def test_empty_string_returns_zero():
+    assert add( "" ) == 0
+
