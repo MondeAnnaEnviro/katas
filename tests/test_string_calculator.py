47094137df7ebb08def9467bda06facb112f144e
Monde Anna <monde.anna@enviro365.co.za>
2025-06-27 08:16:56 +0200

    sum doubley numbered comma delimited string

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -7,3 +7,7 @@ def test_empty_string_returns_zero():
 
 def test_single_number_returned_as_int():
     assert add( "23" ) == 23
+
+
+def test_doubley_numbered_string_returns_sum_as_int():
+    assert add( "2,3" ) == 5
