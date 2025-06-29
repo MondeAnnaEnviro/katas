834942c7b9a79b2c631215746b0015c8fd0172e9
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-29 06:14:24 +0200

    setup: reset kata

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -1,28 +1,2 @@
 def add( numbers: str ) -> int:
-    numbers = standardise_delimiter( numbers )
-
-    nums = [
-        int( num ) if int( num ) <= 1000 else 0
-        for num in numbers.replace( "\n", "," ).split( "," )
-        if numbers
-    ]
-
-    negs = [ str( num ) for num in nums if num < 0 ]
-
-    if negs:
-        negsStr = ", ".join( negs )
-        raise ValueError( f"negatives not allowed: {negsStr}" )
-
-    return sum( nums )
-
-
-def standardise_delimiter( numbers: str ) -> str:
-    numbers.replace( "\n", "," )
-    if "//" != numbers[ :2 ]:
-        return numbers
-
-    idx = numbers.index( "\n" )
-    delimiter = numbers[ 2:idx ].replace( "[", "" ).replace( "]", "" )
-    return numbers[ idx+1: ].replace( delimiter, "," )
-
-
+    ...
