704b5ecee2b835eaaac9bcbad7b0161a1acc9719
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-29 05:59:38 +0200

    negatives throw

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -8,8 +8,16 @@ def add( numbers: str ) -> int:
         delimiters = numbers[ 2:idx ]
         numbers = numbers[ idx+1: ]
 
-    return sum(
-            int( num )
-            for num in re.split( delimiters, numbers )
-            if numbers
-    )
+    nums = [
+        int( num )
+        for num in re.split( delimiters, numbers )
+        if numbers
+    ]
+
+    negs = [ str( num ) for num in nums if num < 0 ]
+
+    if negs:
+        negsStr = ", ".join( negs )
+        raise ValueError( f"negatives not allowed: {negsStr}" )
+
+    return sum( nums )
