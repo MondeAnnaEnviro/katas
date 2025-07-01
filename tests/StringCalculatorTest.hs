87a23a4c0abe3cefcb7cf7411509e5900d518531
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-01 10:25:53 +0200

    =============================== R E S E T ===================================

--- tests/StringCalculatorTest.hs
+++ tests/StringCalculatorTest.hs
@@ -5,24 +5,6 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "sum string:" $ do
-    it "empty string renders zero" $ do
-      add "" `shouldBe` 0
-
-    it "string of value n renders n" $ do
-      add "10" `shouldBe` 10
-
-    it "sum string of two comma seperated numbers" $ do
-      add "1,1" `shouldBe` 2
-
-    it "sum string of n comma seperated numbers" $ do
-      add "0,1,1,2,3,5,8" `shouldBe` 20
-
-    it "newline as delimiter" $ do
-      add "0,1\n2" `shouldBe` 3
-
-{-
-  describe "sum string with user delim:" $ do
-    it "single delim" $ do
-      add "//;\n1;2" `shouldBe` 3
--}
+  describe "should pass" $ do
+    it "passes" $ do
+      0 `shouldBe` 0
