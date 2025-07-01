8e0bf3b0404cdc149562e61844a3480e6195ccb9
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-01 09:45:46 +0200

    proj reset

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -1,32 +1,4 @@
 
 
 def add( numbers: str ) -> int:
-    numbers = standardise_delimiter( numbers )
-    nums = [
-        int( num ) if int( num ) <= 1000 else 0
-        for num in numbers.split( "," )
-        if numbers
-    ]
-
-    negs = [ str( num ) for num in nums if num < 0 ]
-
-    if negs:
-        negsStr = ", ".join( negs )
-        raise ValueError( f"negatives not allowed: {negsStr}" )
-
-    return sum( nums )
-
-
-def standardise_delimiter( numbers: str ) -> str:
-    if numbers[ :2 ] != "//":
-        return numbers.replace( "\n", "," )
-
-    idx = numbers.index( "\n" )
-    delims = numbers[ 2:idx ].replace( "][", "," ).replace( "[", "" ).replace( "]", "" )
-    numbers = numbers[ idx+1: ]
-
-    for delim in delims.split( "," ):
-        numbers = numbers.replace( delim, "," )
-
-    return numbers
-
+    ...
