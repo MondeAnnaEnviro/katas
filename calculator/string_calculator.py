04b606a8722ceafa59eb0b7c591a5c28c099c583
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-01 09:28:56 +0200

    refactor: extract standardising delimiter

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -1,11 +1,15 @@
 
 
 def add( numbers: str ) -> int:
+    numbers = standardise_delimiter( numbers )
+    return sum( int( num ) for num in numbers.split( "," ) if numbers )
 
+
+def standardise_delimiter( numbers: str ) -> str:
     if numbers[ :2 ] == "//":
         idx = numbers.index( "\n" )
         delims = numbers[ 2:idx ]
         numbers = numbers[ idx+1: ].replace( delims, "," )
     numbers = numbers.replace( "\n", "," )
-    return sum( int( num ) for num in numbers.split( "," ) if numbers )
+    return numbers
 
