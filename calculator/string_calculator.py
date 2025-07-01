9bd50d17896fd7c6177ff7575a5cf3eb79473958
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-01 09:27:17 +0200

    parse user delim

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -1,6 +1,11 @@
 
 
 def add( numbers: str ) -> int:
+
+    if numbers[ :2 ] == "//":
+        idx = numbers.index( "\n" )
+        delims = numbers[ 2:idx ]
+        numbers = numbers[ idx+1: ].replace( delims, "," )
     numbers = numbers.replace( "\n", "," )
     return sum( int( num ) for num in numbers.split( "," ) if numbers )
 
