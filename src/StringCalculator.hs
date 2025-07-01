4c514b817440d8c53c5cb523905b388775c590d8
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-01 10:18:25 +0200

    have newline as delimiter

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -6,4 +6,40 @@ import Data.List.Split
 
 add :: String -> Int
 add "" = 0
-add numbers = sum [ read num :: Int | num <- splitOn "," numbers ]
+--add numbers = sum . multiSplit . intList . numsList $ numbers
+-- add numbers = sum . multiSplit . intList .
+add numbers = sum . intList . numsList $ numbers
+
+
+getBody :: String -> String
+getBody numbers
+  | not . hasFlag $ numbers = numbers
+  | otherwise = last . splitOn "\n" $ numbers
+
+
+getDelimiter :: String -> [String]
+getDelimiter numbers
+  | not . hasFlag $ numbers = [",", "\n"]
+  | otherwise = [ head . splitOn "\n" $ numbers ]
+
+
+hasFlag :: String -> Bool
+hasFlag numbers = take 2 numbers == "//"
+
+
+intList :: [String] -> [Int]
+intList numbers = [ read num :: Int | num <- numbers ]
+
+
+multiSplit :: (Eq a) => [[a]] -> [a] -> [[a]]
+multiSplit delims nums = foldl (\xs d -> xs >>= splitOn d ) [nums] delims
+
+
+numsList :: String -> [String]
+numsList numbers = multiSplit ( getDelimiter numbers )( getBody numbers )
+
+
+standardiseDelimiter :: String -> String
+standardiseDelimiter numbers
+  | not . hasFlag $ numbers = numbers
+  | otherwise = ","
--- tests/StringCalculatorTest.hs
+++ tests/StringCalculatorTest.hs
@@ -17,3 +17,12 @@ main = hspec $ do
 
     it "sum string of n comma seperated numbers" $ do
       add "0,1,1,2,3,5,8" `shouldBe` 20
+
+    it "newline as delimiter" $ do
+      add "0,1\n2" `shouldBe` 3
+
+{-
+  describe "sum string with user delim:" $ do
+    it "single delim" $ do
+      add "//;\n1;2" `shouldBe` 3
+-}
