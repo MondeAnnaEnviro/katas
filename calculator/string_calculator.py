7e1eeaf6f62c887ad3a755c21e4e10087f3daefd
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-02 08:43:12 +0200

    refactor: append newline replacement to comprehension

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -1,5 +1,8 @@
 
 
 def add( numbers: str ) -> int:
-    numbers = numbers.replace( "\n", "," )
-    return sum( int( num ) for num in numbers.split( "," ) if numbers )
+    return sum(
+        int( num )
+        for num in numbers.replace( "\n", "," ).split( "," )
+        if numbers
+    )
