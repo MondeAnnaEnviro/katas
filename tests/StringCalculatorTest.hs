a6849761dba3a2466b75f08886517309e1c7ed58
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-02 22:18:09 +0200

    =============================== R E S E T ===================================

--- tests/StringCalculatorTest.hs
+++ tests/StringCalculatorTest.hs
@@ -5,40 +5,4 @@ import Control.Exception
 import Test.Hspec
 
 main :: IO ()
-main = hspec $ do
-  describe "sum delimited numbers:" $ do
-    it "empty string sums to zero" $ do
-      add "" `shouldBe` 0
-
-    it "one number returned as int" $ do
-      add "6" `shouldBe` 6
-
-    it "two numbers are summed" $ do
-      add "54,3" `shouldBe` 57
-
-    it "n numbers are summed" $ do
-      add "6,5,4,3" `shouldBe` 18
-
-    it "newline as delimiter" $ do
-      add "1,2\n3" `shouldBe` 6
-
-    it "ignore numbers greater that one thousand" $ do
-      add "1,1001" `shouldBe` 1
-
-  describe "parse user provided delimiters:" $ do
-    it "single delimiter" $ do
-      add "//(\n9(8" `shouldBe` 17
-
-    it "var len delimiter" $ do
-      add "//[^^^]\n6^^^6^^^6" `shouldBe` 18
-
-    it "multiple delimiters" $ do
-      add "//[^][%]\n6^66%6" `shouldBe` 78
-
-    it "multiple var len delimiters" $ do
-      add "//[^][%%][!!!]\n6^6!!!6%%6" `shouldBe` 24
-
-  describe "exception handling:" $ do
-    it "negatives throw error" $ do
-      let message = "negatives not allowed: [-44,-2]"
-      evaluate ( add "-44,-2,4" ) `shouldThrow` errorCall message
+main = putStrLn "\n\nran\n\n"
