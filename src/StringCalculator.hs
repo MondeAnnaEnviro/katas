e5b8fa2d1aaa63994b34f66b25ea302c2ded8284
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-02 15:22:36 +0200

    single number returned as int

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -7,3 +7,4 @@ import Data.List.Split
 
 add :: String -> Int
 add "" = 0
+add number = read number :: Int
--- tests/StringCalculatorTest.hs
+++ tests/StringCalculatorTest.hs
@@ -10,3 +10,6 @@ main = hspec $ do
   describe "sum delimited numbers:" $ do
     it "empty string returns zero" $ do
       add "" `shouldBe` 0
+
+    it "single numbers returned as int" $ do
+      add "555" `shouldBe` 555
