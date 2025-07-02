b553e2d5850e00aad7b1e3a3c0f5723a2822282d
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-02 08:39:58 +0200

    one number

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -1,4 +1,4 @@
 
 
 def add( numbers: str ) -> int:
-    ...
+    return int( numbers ) if numbers else 0
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -1,3 +1,11 @@
 from calculator.string_calculator import add
 import pytest
 
+
+
+def test_empty_str():
+    assert add( "" ) == 0
+
+
+def test_one_number():
+    assert add( "44" ) == 44
