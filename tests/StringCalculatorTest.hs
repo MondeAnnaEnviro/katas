cff32d9eb110e526d6e26244e9ad1d9659f21a2e
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-02 16:19:26 +0200

    n numbered string returns as sum

--- tests/StringCalculatorTest.hs
+++ tests/StringCalculatorTest.hs
@@ -15,3 +15,6 @@ main = hspec $ do
 
     it "two numbered string returns as sum" $ do
       add "1,0" `shouldBe` 1
+
+    it "n numbered string returns as sum" $ do
+      add "1,0,2,1,3" `shouldBe` 7
