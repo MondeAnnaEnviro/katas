7e48d529c98b11323f30b4915d1106d285e258f2
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-03 06:04:50 +0200

    one number returned as int

--- tests/StringCalculatorTest.hs
+++ tests/StringCalculatorTest.hs
@@ -9,3 +9,6 @@ main = hspec $ do
   describe "sum delimiters numbers:" $ do
     it "empty string results in zero" $ do
       add "" `shouldBe` 0
+
+    it "one number returned as int" $ do
+      add "3" `shouldBe` 3
