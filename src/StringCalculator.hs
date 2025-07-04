8ca93eab37b82ddf8a4d33c4166b5a6a8df2c59e
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-07-04 18:09:52 +0200

    session summary (00:18): opted to execute without commits

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -2,3 +2,64 @@ module StringCalculator where
 
 
 import Data.List.Split
+
+
+add :: String -> Int
+add "" = 0
+add numbers = total . parseNegs . intList . numsList $ numbers
+
+
+dropBrackets :: String -> String
+dropBrackets delims
+  | not . hasBrackets $ delims = delims
+  | otherwise = init . tail $ delims
+
+
+getBody :: String -> String
+getBody numbers
+  | not . hasFlag $ numbers = numbers
+  | otherwise = last . splitOn "\n" $ numbers
+
+
+getDelims :: String -> [String]
+getDelims numbers
+  | not . hasFlag $ numbers = [",", "\n"]
+  | otherwise = multiSplit ["]["] $ dropBrackets $ drop 2 $ head . splitOn "\n" $ numbers
+
+
+getNegs :: [Int] -> [Int]
+getNegs integers = filter ( < 0 ) integers
+
+
+hasBrackets :: String -> Bool
+hasBrackets delims = head delims == '[' && last delims == ']'
+
+
+hasFlag :: String -> Bool
+hasFlag numbers = take 2 numbers == "//"
+
+
+hasNegs :: [Int] -> Bool
+hasNegs integers = any ( < 0 ) integers
+
+
+intList :: [String] -> [Int]
+intList numbers = [read num :: Int | num <- numbers ]
+
+
+multiSplit :: [String] -> String -> [String]
+multiSplit delims nums = foldl (\ xs d -> xs >>= splitOn d ) [nums] delims
+
+
+numsList :: String -> [String]
+numsList numbers = multiSplit ( getDelims numbers )( getBody numbers )
+
+
+parseNegs :: [Int] -> [Int]
+parseNegs integers
+  | not . hasNegs $ integers = integers
+  | otherwise = error $ "negatives not allowed: " ++ show ( getNegs integers )
+
+
+total :: [Int] -> Int
+total integers = sum . filter ( <= 1000 ) $ integers
