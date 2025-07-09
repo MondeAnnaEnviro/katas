cdda433e409285046536757c6eae9dfb127fcdb7
MondeAnna <monde.anna@enviro365.co.za>
2025-07-09 18:31:51 +0200

    session summary (00:19): feels a bit smooth

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -2,3 +2,70 @@ module StringCalculator where
 
 
 import Data.List.Split
+
+
+add :: String -> Int
+add "" = 0
+add numbers = sum . thousandOrLess . processNegs . intList . numsList $ numbers
+
+
+dropBrackets :: String -> String
+dropBrackets delims
+  | not . hasBrackets $ delims = delims
+  | otherwise = init . tail $ delims
+
+
+dropFlag :: String -> String
+dropFlag delims
+  | not . hasFlag $ delims = delims
+  | otherwise = drop 2 delims
+
+
+getBody :: String -> String
+getBody numbers
+  | not . hasFlag $ numbers = numbers
+  | otherwise = last . splitOn "\n" $ numbers
+
+
+getDelims :: String -> [String]
+getDelims numbers
+  | not . hasFlag $ numbers = [",", "\n"]
+  | otherwise = splitOn "][" $ dropBrackets . dropFlag . head . splitOn "\n" $ numbers
+
+
+getNegs :: [Int] -> [Int]
+getNegs integers = filter ( < 0 ) integers
+
+
+hasBrackets :: String -> Bool
+hasBrackets delims = head delims == '[' && last delims == ']'
+
+
+hasFlag :: String -> Bool
+hasFlag numbers = take 2 numbers == "//"
+
+
+hasNegs :: [Int] -> Bool
+hasNegs integers = any ( < 0 ) integers
+
+
+intList :: [String] -> [Int]
+intList numbers = [ read num :: Int | num <- numbers ]
+
+
+multiSplit :: [String] -> String -> [String]
+multiSplit delims nums = foldl (\ xs d -> xs >>= splitOn d ) [nums] delims
+
+
+numsList :: String -> [String]
+numsList numbers = multiSplit ( getDelims numbers )( getBody numbers )
+
+
+processNegs :: [Int] -> [Int]
+processNegs integers
+  | not . hasNegs $ integers = integers
+  | otherwise = error $ "negatives not allowed: " ++ show ( getNegs integers )
+
+
+thousandOrLess :: [Int] -> [Int]
+thousandOrLess integers = filter ( <= 1000 ) integers
