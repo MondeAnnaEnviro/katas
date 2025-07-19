e38489110492605712572184805122603ed0beea
MondeAnna <monde.anna@enviro365.co.za>
2025-07-19 19:05:39 +0200

    session summary (00:16)

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -1,4 +1,37 @@
 
 
 def add( numbers: str ) -> int:
-    ...
+    numbers = standardise_delims( numbers )
+
+    nums = [
+        thousandOrLess( num )
+        for num in numbers.split( "," )
+        if numbers
+    ]
+
+    negs = [ str( num ) for num in nums if num < 0 ]
+
+    if negs:
+      negsStr = ", ".join( negs )
+      raise ValueError( f"negatives not allowed: {negsStr}" )
+
+    return sum( nums )
+
+
+def standardise_delims( numbers: str ) -> str:
+    if numbers[ :2 ] != "//":
+        return numbers.replace( "\n", "," )
+
+    idx = numbers.index( "\n" )
+    delims = numbers[ 2:idx ].replace( "][", "," ).replace( "[", "" ).replace( "]", "" )
+    numbers = numbers[ idx+1: ]
+
+    for delim in delims.split( "," ):
+        numbers = numbers.replace( delim, "," )
+
+    return numbers
+
+
+def thousandOrLess( number: str ) -> int:
+    return int( number ) if int( number ) <= 1000 else 0
+
