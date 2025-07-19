c41e4bbc56bdf3a2f3e575b0e84e0584e277c603
MondeAnna <monde.anna@enviro365.co.za>
2025-07-19 21:55:06 +0200

    session summary: 00:14

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -1,2 +1,48 @@
 from calculator.string_calculator import add
 import pytest
+
+
+def test_empty_string():
+    assert add( "" ) == 0
+
+
+def test_one_number():
+    assert add( "1" ) == 1
+
+
+def test_two_numbers():
+    assert add( "1,2" ) == 3
+
+
+def test_n_numbers():
+    assert add( "1,2,3" ) == 6
+
+
+def test_newline():
+    assert add( "0,1\n2" ) == 3
+
+
+def test_ignore_numbers_greater_than_one_thousand():
+    assert add( "1,1001" ) == 1
+
+
+def test_user_delim():
+    assert add( "//;\n1;2" ) == 3
+
+
+def test_var_len_user_delim():
+    assert add( "//[;;]\n1;;2" ) == 3
+
+
+def test_multiple_user_delims():
+    assert add( "//[!][@]\n1!2@3" ) == 6
+
+
+def test_multiple_var_len_user_delims():
+    assert add( "//[!][@@][###]\n1!2@@3###4" ) == 10
+
+
+def test_negatives_throws():
+    match = "negatives not allowed: -1, -2"
+    with pytest.raises( ValueError, match=match ):
+        add( "-1,-2" )
