f88915cb0a15e0eea9c7162b564bfc25efebe22a
Monde Anna <monde.anna@enviro365.co.za>
2025-07-22 16:38:50 +0200

    session summary: (00:11)

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -2,39 +2,35 @@ from typing import List
 
 
 def add( numbers: str ) -> int:
-    numbers = standardise_delimiter( numbers )
-    nums = get_list_of_numbers( numbers )
+    numbers = standardise_delims( numbers )
+
+    nums = [
+        thousandOrLess( num )
+        for num in numbers.split( "," )
+        if numbers
+    ]
 
     negs = [ str( num ) for num in nums if num < 0 ]
 
     if negs:
         negsStr = ", ".join( negs )
-        raise ValueError( f"negatives not allowed: {negsStr}" )
+        raise ValueError( f"negatives not allowed: {negsStr}")
 
     return sum( nums )
 
 
-def get_list_of_numbers( numbers: str ) -> List[int]:
-    return [
-        thousand_or_less( num )
-        for num in numbers.split( "," )
-        if numbers
-    ]
+def standardise_delims( numbers: str ) -> str:
+    if not numbers.startswith("//"):
+        return numbers.replace("\n", ",")
 
+    idx = numbers.index("\n")
+    delims = numbers[ 2:idx ].replace("][", ",").replace( "[", "").replace("]", "")
+    numbers = numbers[idx+1:]
 
-def standardise_delimiter( numbers: str ) -> str:
-    if not numbers.startswith( "//"):
-        return  numbers.replace( "\n", "," )
-
-    idx = numbers.index( "\n" )
-    delims = numbers[ 2:idx ].replace( "][", "," ).replace( "[", "" ).replace( "]", "")
-    numbers = numbers[ idx+1: ]
-
-    for delim in delims.split( "," ):
-        numbers = numbers.replace( delim, "," )
-
+    for delim in delims.split(","):
+        numbers = numbers.replace( delim, ",")
     return numbers
 
 
-def thousand_or_less( number: str ) -> int:
-    return int( number ) if int( number ) <= 1000 else 0
+def thousandOrLess( num: str ) -> str:
+    return int( num ) if int( num ) <= 1000 else 0
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -9,7 +9,6 @@ def test_empty_string():
 def test_one_number():
     assert add( "1" ) == 1
 
-
 def test_two_numbers():
     assert add( "1,2" ) == 3
 
@@ -19,30 +18,25 @@ def test_n_numbers():
 
 
 def test_newline():
-    assert add( "0,1\n2" ) == 3
+    assert add( "1,2\n3" ) == 6
 
+
+def test_user_delim():
+    assert add("//;\n1;2") == 3
+
+def test_negatives_throw():
+    match = "negatives not allowed: -1, -2"
+    with pytest.raises( ValueError, match=match):
+        add( "-1,-2" )
 
 def test_ignore_numbers_greater_than_1000():
     assert add( "1,1001" ) == 1
 
+def test_var_len_user_delim():
+    assert add("//[!!]\n1!!2") == 3
 
-def test_user_delimiter():
-    assert add( "//;\n1;2" ) == 3
+def test_user_delims():
+    assert add("//[!][@]\n1!2@3") == 6
 
-
-def test_var_len_user_delimiter():
-    assert add( "//[;;]\n1;;2" ) == 3
-
-
-def test_multiple_user_delimiters():
-    assert add( "//[!][@]\n1!2@3" ) == 6
-
-
-def test_multiple_var_len_user_delimiters():
-    assert add( "//[!][@@][###]\n1!2@@3###4" ) == 10
-
-
-def test_negatives_throw():
-    match = "negatives not allowed: -1, -2"
-    with pytest.raises( ValueError, match=match ):
-        add( "-1,-2" )
+def test_user_delims():
+    assert add("//[!][@@][###]\n1!2@@3###4") == 10
