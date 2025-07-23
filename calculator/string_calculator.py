be8a4ca6d065f1fbab2087ceb9be50f86fbe00b9
Monde Anna <monde.anna@enviro365.co.za>
2025-07-23 15:05:18 +0200

    session summary: 00:13

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -2,4 +2,33 @@ from typing import List
 
 
 def add( numbers: str ) -> int:
-    ...
+    numbers = standardise_numbers( numbers )
+    nums = [
+        thousand_or_less( num ) for num in numbers.split(",") if numbers
+    ]
+    eval_negatives( nums=nums )
+    return sum( nums )
+
+
+def thousand_or_less( num: str ) -> int:
+    return int(num) if int( num ) <= 1000 else 0
+
+
+def eval_negatives( nums: List[int] ) -> None:
+    negs = [ str(num) for num in nums if num < 0 ]
+    if negs:
+        negsStr = ", ".join( negs )
+        raise ValueError( f"negatives not allowed: {negsStr}" )
+
+
+def standardise_numbers( numbers: str ) -> str:
+    if not numbers.startswith("//"):
+        return numbers.replace("\n", ",") 
+    
+    idx = numbers.index("\n")
+    delims = numbers[2:idx].replace("][", ",").replace("[", "").replace("]", "")
+    numbers = numbers[idx+1:]
+
+    for delim in delims.split(","):
+        numbers = numbers.replace(delim, ",")
+    return numbers
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -1,2 +1,38 @@
 from calculator.string_calculator import add
 import pytest
+
+
+def test_empty_str():
+    assert add("") == 0
+
+def test_one_number():
+    assert add("2") == 2
+
+def test_two_numbers():
+    assert add("1,2") == 3
+
+def test_n_numbers():
+    assert add("1,2,3") == 6
+
+def test_newline():
+    assert add("1,2\n3") == 6
+
+def test_numbers_greater_than_1000():
+    assert add("1,1001") == 1
+
+def test_user_delim():
+    assert add("//;\n1;2") == 3
+
+def test_var_len_user_delims():
+    assert add("//[;;]\n1;;2") == 3
+
+def test_multiple_user_delims():
+    assert add("//[!][@]\n1!2@3") == 6
+
+def test_multiple_user_delims():
+    assert add("//[!][@@][###]\n1!2@@3###4") == 10
+
+def test_negative_raise():
+    match_ = "negatives not allowed: -1, -2"
+    with pytest.raises( ValueError, match=match_ ):
+        add("-1,-2")
