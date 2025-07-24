e26af3fed740f1130f36e5490d213523f79d5543
Monde Anna <monde.anna@enviro365.co.za>
2025-07-24 06:38:09 +0200

    session summary: 00:12

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -1,5 +1,32 @@
 from typing import List
 
 
-def add( numbers: str ) -> int:
-    ,,,
+def add(numbers: str) -> int:
+    numbers = standardise_delims(numbers)
+
+    nums = [thousand_or_less(num) for num in numbers.split(",") if numbers]
+
+    negs = [str(num) for num in nums if num < 0]
+
+    if negs:
+        negsStr = ", ".join(negs)
+        raise ValueError(f"negatives not allowed: {negsStr}")
+
+    return sum(nums)
+
+
+def standardise_delims(numbers: str) -> str:
+    if not numbers.startswith("//"):
+        return numbers.replace("\n", ",")
+
+    idx = numbers.index("\n")
+    delims = numbers[2:idx].replace("][", ",").replace("]", "").replace("[", "")
+    numbers = numbers[idx + 1 :]
+
+    for delim in delims.split(","):
+        numbers = numbers.replace(delim, ",")
+    return numbers
+
+
+def thousand_or_less( num: str ) -> int:
+    return int(num) if int(num) <= 1000 else 0
